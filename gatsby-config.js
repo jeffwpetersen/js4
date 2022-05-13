2c13737e3b595aa97ca82bb855e9184bf29f2c94
jeffwpetersen <jeff@juniperstudio.com>
2022-05-13 11:26:29 -0600

    Google updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,27 +16,21 @@ module.exports = {
     `gatsby-transformer-sharp`,
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-smoothscroll",
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-        purgeCSSOptions: {
-          // https://purgecss.com/configuration.html#options
-          // safelist: ['safelist'], // Don't remove this selector
-        },
-        // More options defined here https://purgecss.com/configuration.html#options
-      },
-    },
-    {
-    resolve: 'gatsby-plugin-google-analytics',
+  {
+    resolve: `gatsby-plugin-purgecss`,
     options: {
-      "trackingId": "UA-9390772-1"
-    }
-  }, "gatsby-plugin-sitemap", {
+      printRejected: true, // Print removed selectors and processed file names
+      // develop: true, // Enable while using `gatsby develop`
+      tailwind: true, // Enable tailwindcss support
+      // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+      // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      purgeCSSOptions: {
+        // https://purgecss.com/configuration.html#options
+        // safelist: ['safelist'], // Don't remove this selector
+      },
+      // More options defined here https://purgecss.com/configuration.html#options
+    },
+  }, {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
@@ -67,6 +61,37 @@ module.exports = {
           include: /\.inline\.svg$/
         }
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingIds: ['315047965', 'UA-9390772-1'],
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "OPT-MCZ8PXD",
+        // Enables Google Optimize Experiment ID
+        experimentId: "iBxQ2alUQTG5Zq87Raem3A",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
     },
   ]
 };
